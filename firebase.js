b4764c5143a63a00a6290a5ef0b2a600cda80fbc
linkd1146-creator <linkd1146@gmail.com>
2026-05-02 18:03:35 -0700

    Update Firebase configuration with new project details

--- firebase.js
+++ firebase.js
@@ -2,10 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT"
+  apiKey: "AIzaSyDRbZaikbDC2GuWIfL7OWalXY81nvSd_DA",
+  authDomain: "minitok-bd7c1.firebaseapp.com",
+  projectId: "minitok-bd7c1",
+  storageBucket: "minitok-bd7c1.firebasestorage.app",
+  messagingSenderId: "507499794732",
+  appId: "1:507499794732:web:6f97c616d022e8beb79c89"
 };
 
 const app = initializeApp(firebaseConfig);
